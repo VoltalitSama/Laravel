81e6e7f026edca1ab697862d1982d1ebbe80d952
ThibaudDufour <thibaud-dufour@sfr.fr>
2023-12-01 23:40:28 +0100

    TD10 - Event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,7 +17,7 @@ class EventServiceProvider extends ServiceProvider
     /**
      * The event to listener mappings for the application.
      *
-     * @var array<class-string, array<int, class-string>>
+     * @var array<class-string, array<int,  class-string>>
      */
     protected $listen = [
 
